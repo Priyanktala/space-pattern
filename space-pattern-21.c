073a2440773933a5b76b8149bfd92e15a7f66826
Priyanktala <131126830+Priyanktala@users.noreply.github.com>
2023-08-17 11:31:37 -0400

    Add files via upload

---
+++ space-pattern-21.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+main()
+{
+	int i,j,k;
+	
+	for(i=1; i<=5; i++)
+	{
+		for(k=1; k<i; k++)
+		{
+			printf(" ");
+		}
+		for(j=5;j>=i;j--)
+		{
+			if(i%2==1)
+			{
+			printf("1");
+			}
+			else
+			{
+			printf("0");
+			}
+			
+		}
+		printf("\n");
+	}
+}
